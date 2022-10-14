adf620fecdb8eebcbb84100810a7e89f02822d61
daveclinton <clintondavid46@gmail.com>
2022-10-14 23:26:20 +0300

    chore: Add app main data

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,83 @@
 import React from "react";
-import logo from "./logo.svg";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AddProject from "./components/AddProject";
+import EditProject from "./components/EditProject";
+import ProjectList from "./components/ProjectList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={"/create-student"} className="nav-link">
+                  React MERN Stack App
+                </Link>
+              </Navbar.Brand>
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={"/create-student"} className="nav-link">
+                    Add Project REF
+                  </Link>
+                </Nav>
+                <Nav>
+                  <Link to={"/student-list"} className="nav-link">
+                    Add Project Name
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    component={(props: JSX.IntrinsicAttributes) => (
+                      <AddProject {...props} />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/create-student"
+                    component={(props: JSX.IntrinsicAttributes) => (
+                      <AddProject {...props} />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/edit-student/:id"
+                    component={(props: JSX.IntrinsicAttributes) => (
+                      <EditProject {...props} />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/student-list"
+                    component={(props: JSX.IntrinsicAttributes) => (
+                      <ProjectList {...props} />
+                    )}
+                  />
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     </div>
   );
 }
-
 export default App;
